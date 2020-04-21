cd9f9ae51f6c1b574b68d2d49228caf7bda200be
vedanshgarg <vedansh.garg.7@gmail.com>
2020-04-21 22:16:05 +0530

    navbar fix

--- content/src/scripts/containers/Resizer/Resizer.jsx
+++ content/src/scripts/containers/Resizer/Resizer.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import { setWidth } from "../../../../../event/src/actions/UI";
 import { connect } from "react-redux";
 
 const Resizer = ({ width, setWidth }) => {
-  const mouseDownListener = (event) => {
+  const [transitionProps, setTransitionProps] = useState("");
+
+  const mouseDownListener = () => {
     document.body.style.userSelect = "none";
+    setTransitionProps(document.querySelector(".nav-sidebar").style.transition);
+    document.querySelector(".nav-sidebar").style.transition = "none";
     document.addEventListener("mousemove", mouseMoveListener);
     document.addEventListener("mouseup", mouseUpListener);
   };
 
   const mouseUpListener = () => {
     document.body.style.userSelect = "auto";
+    document.querySelector(".nav-sidebar").style.transition = transitionProps;
     document.removeEventListener("mousemove", mouseMoveListener);
     document.removeEventListener("mouseup", mouseUpListener);
   };
