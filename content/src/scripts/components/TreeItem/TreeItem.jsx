09697626b8ddba99f26a460d80c90dbd75684fbd
tavyandy97 <tavy.andy97@gmail.com>
2020-04-26 21:34:17 +0530

    Basic working lazy loading

--- content/src/scripts/components/TreeItem/TreeItem.jsx
+++ content/src/scripts/components/TreeItem/TreeItem.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 
-import { fetchURLDetails } from "../../utils/url";
-
+import { refreshPage } from "../../utils/refreshPage";
 import fileIcons from "../../utils/file-icons";
-import axios from "axios";
 
 import "./styles.css";
 
 const importFileIconCSS = `chrome-extension://${chrome.runtime.id}/libs/file-icon.css`;
 
 function TreeItem({ name, isTree, path, close, open, children }) {
-  const URLDetails = fetchURLDetails();
-
   const handleClick = (path, open, close, isTree) => {
     if (isTree) {
       if (isTree.isOpen) {
@@ -20,37 +16,7 @@ function TreeItem({ name, isTree, path, close, open, children }) {
         open(path);
       }
     } else {
-      const URL = `https://www.gitlab.com/${URLDetails.dirFormatted}/blob/${
-        URLDetails.branchName
-      }/${path.join("/")}`;
-      axios
-        .get(URL)
-        .then((res) => {
-          history.pushState(
-            URL,
-            "",
-            `/${URLDetails.dirFormatted}/blob/${
-              URLDetails.branchName
-            }/${path.join("/")}`
-          );
-          let el = document.createElement("html");
-          el.innerHTML = res.data;
-
-          const anchor = document.createElement("div");
-          anchor.id = "rcr-anchor";
-          anchor.innerHTML = document.getElementById("rcr-anchor").innerHTML;
-          el.querySelector(".layout-page").insertBefore(
-            anchor,
-            el.querySelector(".layout-page").childNodes[0]
-          );
-
-          document.querySelector("body").innerHTML = el.querySelector(
-            "body"
-          ).innerHTML;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      refreshPage(path);
     }
   };
 
