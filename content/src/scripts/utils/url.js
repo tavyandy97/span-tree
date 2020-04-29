b856ee7102c8e3610d1b4f6068f24f43fac03f9f
Vedansh Garg <40305225+vedanshgarg@users.noreply.github.com>
2020-04-29 14:49:11 +0530

    Mark active (#9)
    * dir opening working
    
    * basic func added
    
    * add reloaded state
    
    * remove url from name
    
    * remove console logs
    
    * refactor var
    
    * display none
    
    * remove loader when rendering
    
    * setRendering true
    
    * make instantanious
    
    * fix root list bug
    
    * branch name revert
    
    * applyClosedPageStyling
    
    * move functions inside treeitem

--- content/src/scripts/utils/url.js
+++ content/src/scripts/utils/url.js
@@ -7,29 +7,45 @@ export const fetchURLDetails = () => {
   let branchName = document.querySelector(".dropdown-toggle-text")
     ? document.querySelector(".dropdown-toggle-text").innerText
     : "master";
+  let branchNameSplit = branchName
+    .split("/")
+    .filter((pathSub) => pathSub.length !== 0);
+  let branchFound = false;
   let isTreeVisible = true;
+  let treeOrBlobOrHyphenFound = false;
+  let baseRemovedURLItems = [];
   for (let i = 0; i < pathNameSplit.length; i++) {
-    if (pathNameSplit[i] === "-") {
-      if (
-        !(pathNameSplit[i + 1] === "tree" || pathNameSplit[i + 1] === "blob")
-      ) {
-        isTreeVisible = false;
+    if (treeOrBlobOrHyphenFound) {
+      if (!branchFound) {
+        i += branchNameSplit.length;
+        branchFound = true;
       }
-      break;
+      baseRemovedURLItems.push(pathNameSplit[i]);
+    } else {
+      if (pathNameSplit[i] === "-") {
+        treeOrBlobOrHyphenFound = true;
+        i++;
+        if (!(pathNameSplit[i] === "tree" || pathNameSplit[i] === "blob")) {
+          isTreeVisible = false;
+        }
+      } else if (pathNameSplit[i] === "blob" || pathNameSplit[i] === "tree") {
+        treeOrBlobOrHyphenFound = true;
+      } else {
+        dir.push(pathNameSplit[i]);
+      }
     }
-    if (pathNameSplit[i] === "blob" || pathNameSplit[i] === "tree") {
-      break;
-    }
-    dir.push(pathNameSplit[i]);
   }
+  const dirFormatted = dir.join("/");
+  const baseRemovedURL = baseRemovedURLItems.join("/");
 
   return {
     dir,
-    dirFormatted: dir.join("/"),
+    dirFormatted: dirFormatted,
     dirURLParam: dir.join("%2F"),
     branchName,
     branchNameURL: branchName.split("/").join("%2F"),
     isRepo: !(dir[0] === "dashboard" || dir[0] === "explore"),
     isTreeVisible,
+    baseRemovedURL: baseRemovedURL,
   };
 };
