5ad129d739b333571b911ce9a9e74afe505eaf20
vedanshgarg <vedansh.garg.7@gmail.com>
2020-05-03 01:39:23 +0530

    remove consolelogs

--- content/src/scripts/containers/Resizer/Resizer.jsx
+++ content/src/scripts/containers/Resizer/Resizer.jsx
@@ -27,12 +27,8 @@ const Resizer = ({ width, setWidth }) => {
   };
 
   const mouseMoveListener = (event) => {
-    // if (Math.abs(lastPosX - event.clientX) >= 20) {
-    //   lastPosX = event.clientX;
     setWidth(event.clientX);
     console.log(event.clientX);
-    //   console.log(lastPosX);
-    // }
   };
 
   const debouncedMouseMoveListener = debounce(mouseMoveListener, 2);
@@ -48,43 +44,24 @@ const Resizer = ({ width, setWidth }) => {
 };
 
 function debounce(func, wait, immediate) {
-  // 'private' variable for instance
-  // The returned function will be able to reference this due to closure.
-  // Each call to the returned function will share this common timer.
   let timeout;
 
-  // Calling debounce returns a new anonymous function
   return function () {
-    // reference the context and args for the setTimeout function
     let context = this,
       args = arguments;
 
-    // Should the function be called now? If immediate is true
-    //   and not already in a timeout then the answer is: Yes
     let callNow = immediate && !timeout;
 
-    // This is the basic debounce behaviour where you can call this
-    //   function several times, but it will only execute once
-    //   [before or after imposing a delay].
-    //   Each time the returned function is called, the timer starts over.
     clearTimeout(timeout);
 
-    // Set the new timeout
     timeout = setTimeout(function () {
-      // Inside the timeout function, clear the timeout variable
-      // which will let the next execution run when in 'immediate' mode
       timeout = null;
 
-      // Check if the function already ran with the immediate flag
       if (!immediate) {
-        // Call the original function with apply
-        // apply lets you define the 'this' object as well as the arguments
-        //    (both captured before setTimeout)
         func.apply(context, args);
       }
     }, wait);
 
-    // Immediate mode and no wait timer? Execute the function..
     if (callNow) func.apply(context, args);
   };
 }
