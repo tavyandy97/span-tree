6180d2ba1ad498c89c7219062c0282164067e2a8
tavyandy97 <tavy.andy97@gmail.com>
2020-05-03 13:54:55 +0530

    throttle in utils

--- content/src/scripts/containers/Resizer/Resizer.jsx
+++ content/src/scripts/containers/Resizer/Resizer.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 
 import { setWidth } from "../../../../../event/src/actions/UI";
+import { throttle } from "../../utils/throttle";
 
 import "./styles.css";
 
@@ -38,38 +39,6 @@ const Resizer = ({ width, setWidth }) => {
     ></div>
   );
 };
-
-function throttle(func, wait, options) {
-  let context, args, result;
-  let timeout = null;
-  let previous = 0;
-  if (!options) options = {};
-  let later = function () {
-    previous = options.leading === false ? 0 : Date.now();
-    timeout = null;
-    result = func.apply(context, args);
-    if (!timeout) context = args = null;
-  };
-  return function () {
-    let now = Date.now();
-    if (!previous && options.leading === false) previous = now;
-    let remaining = wait - (now - previous);
-    context = this;
-    args = arguments;
-    if (remaining <= 0 || remaining > wait) {
-      if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
-      }
-      previous = now;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining);
-    }
-    return result;
-  };
-}
 
 const mapStateToProps = (state) => {
   return {
