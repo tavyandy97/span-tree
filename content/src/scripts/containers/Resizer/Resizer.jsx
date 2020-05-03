22f80affbbd3fae6210335c0cd6f5ff4f1be7e7d
vedanshgarg <vedansh.garg.7@gmail.com>
2020-05-03 13:39:23 +0530

    throttle added

--- content/src/scripts/containers/Resizer/Resizer.jsx
+++ content/src/scripts/containers/Resizer/Resizer.jsx
@@ -12,20 +12,22 @@ const Resizer = ({ width, setWidth }) => {
     document.body.style.userSelect = "none";
     setTransitionProps(document.querySelector(".nav-sidebar").style.transition);
     document.querySelector(".nav-sidebar").style.transition = "none";
-    document.addEventListener("mousemove", mouseMoveListener);
+    document.addEventListener("mousemove", throttledMouseMoveListener);
     document.addEventListener("mouseup", mouseUpListener);
   };
 
   const mouseUpListener = () => {
     document.body.style.userSelect = "auto";
     document.querySelector(".nav-sidebar").style.transition = transitionProps;
-    document.removeEventListener("mousemove", mouseMoveListener);
+    document.removeEventListener("mousemove", throttledMouseMoveListener);
     document.removeEventListener("mouseup", mouseUpListener);
   };
 
   const mouseMoveListener = (event) => {
     setWidth(event.clientX);
   };
+
+  const throttledMouseMoveListener = throttle(mouseMoveListener, 16);
 
   return (
     <div
@@ -36,6 +38,38 @@ const Resizer = ({ width, setWidth }) => {
     ></div>
   );
 };
+
+function throttle(func, wait, options) {
+  let context, args, result;
+  let timeout = null;
+  let previous = 0;
+  if (!options) options = {};
+  let later = function () {
+    previous = options.leading === false ? 0 : Date.now();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
+  };
+  return function () {
+    let now = Date.now();
+    if (!previous && options.leading === false) previous = now;
+    let remaining = wait - (now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+}
 
 const mapStateToProps = (state) => {
   return {
