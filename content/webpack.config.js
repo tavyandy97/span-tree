97ddbab7ebd4a4bd3265a34ca57dfb7fb612f257
Taveesh Anand <tavy.andy97@gmail.com>
2020-05-03 11:52:31 +0530

    Ta uglify (#13)
    * POPUP removed
    
    * Minify Added
    
    * Optional minify added

--- content/webpack.config.js
+++ content/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = {
   entry: ["./content/src/scripts/index.js"],
@@ -13,6 +14,11 @@ module.exports = {
     extensions: [".js", ".jsx", ".json", ".css", ".svg"],
     modules: ["node_modules"],
   },
+
+  plugins:
+    process.env.CHROME_EXTENSION === "production"
+      ? [new MinifyPlugin({}, {})]
+      : [],
 
   module: {
     loaders: [
