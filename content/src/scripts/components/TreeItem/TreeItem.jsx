7e9f220f16a34f026607a60eb6f2b9dc291431dc
vedanshgarg <vedansh.garg.7@gmail.com>
2020-05-03 18:39:47 +0530

    space file fixed

--- content/src/scripts/components/TreeItem/TreeItem.jsx
+++ content/src/scripts/components/TreeItem/TreeItem.jsx
@@ -45,7 +45,7 @@ function TreeItem({
     if (remainingURL.length != 0) {
       let activeIconName = remainingURL.split("/")[0];
       let urlRemaining = remainingURL.substring(activeIconName.length + 1);
-      if (activeIconName === name) {
+      if (activeIconName.replace(/%20/gi, " ") === name) {
         if (isTree && !isTree.isOpen) {
           isTree.isOpen = true;
           open(path);
@@ -70,7 +70,7 @@ function TreeItem({
     if (remainingURL.length != 0) {
       let activeIconName = remainingURL.split("/")[0];
       let urlRemaining = remainingURL.substring(activeIconName.length + 1);
-      if (activeIconName === name) {
+      if (activeIconName.replace(/%20/gi, " ") === name) {
         if (urlRemaining.length === 0) {
           isItemActive = true;
         }
