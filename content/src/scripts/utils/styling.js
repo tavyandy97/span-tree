a36e1316addb3fb6b0cd6e6e5259aa87a3d6e2cb
Taveesh Anand <tavy.andy97@gmail.com>
2020-05-24 13:39:42 +0530

    null check

--- content/src/scripts/utils/styling.js
+++ content/src/scripts/utils/styling.js
@@ -2,7 +2,7 @@ export const applyOpenedPageStyling = (width) => {
   document.querySelector("header").style.left = width + "px";
   document.querySelector("body").style.marginLeft = width + "px";
   document.querySelector("body").style.overflowX = "auto";
-  if(document.querySelector(".nav-sidebar") !== null){
+  if (document.querySelector(".nav-sidebar")) {
     document.querySelector(".nav-sidebar").style.left = width + "px";
   }
 };
@@ -10,7 +10,7 @@ export const applyOpenedPageStyling = (width) => {
 export const applyClosedPageStyling = () => {
   document.querySelector("header").style.left = "0";
   document.querySelector("body").style.marginLeft = "0";
-  if(document.querySelector(".nav-sidebar") !== null){
+  if (document.querySelector(".nav-sidebar")) {
     document.querySelector(".nav-sidebar").style.left = "0";
   }
 };
