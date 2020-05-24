4a5c158358142fd154d2d104d9b5a11268bb6437
Taveesh Anand <tavy.andy97@gmail.com>
2020-05-24 19:08:27 +0530

    themeList refactor

--- content/src/scripts/index.js
+++ content/src/scripts/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { Store, applyMiddleware } from "webext-redux";
-import {browserKey} from "./utils/browser";
+import { browserKey } from "./utils/browser";
 import thunkMiddleware from "redux-thunk";
 import App from "./containers/app/App";
-import {isPresentInThemeList, switchTheme} from "./utils/themeList";
+import { isPresentInThemeList, switchTheme } from "./utils/themeList";
 
 const proxyStore = new Store();
 const middleware = [thunkMiddleware];
@@ -20,11 +20,11 @@ darkGitlab.href = `${browserKey()}-extension://${chrome.i18n.getMessage(
   "@@extension_id"
 )}/libs/gitlab-dark.css`;
 document
-  .querySelector("head")
-  .insertBefore(darkGitlab, document.querySelector("head").childNodes[0]);
+  .querySelector("body")
+  .insertBefore(darkGitlab, document.querySelector("body").childNodes[0]);
 setTimeout(function () {
-  switchTheme()
-},10000);
+  switchTheme();
+}, 10000);
 
 const anchor = document.createElement("div");
 anchor.id = "rcr-anchor";
--- content/src/scripts/utils/themeList.js
+++ content/src/scripts/utils/themeList.js
@@ -1,42 +1,22 @@
-export const addDomainToThemeList = () => {
-  const domain = location.origin;
-  let themeList = localStorage.getItem("spantree-themelist");
-  if(themeList == null){
-    themeList = [];
-  }else{
-    themeList = themeList.split(",");
-  }
-  themeList.push(domain);
-  localStorage.setItem("spantree-themelist", themeList.toString());
-};
-
 export const isPresentInThemeList = () => {
   const domain = location.origin;
-  let themeList = localStorage.getItem("spantree-themelist");
-  if(themeList == null){
-    themeList = [];
-  }else{
-    themeList = themeList.split(",");
-  }
-  return themeList.includes(domain);
+  let themeList = JSON.parse(localStorage.getItem("spantree-themelist")) || {};
+
+  return domain in themeList && themeList[domain];
 };
 
 export const switchTheme = () => {
   const domain = location.origin;
-  let themeList = localStorage.getItem("spantree-themelist");
-  if(themeList == null){
-    themeList = [];
-  }else{
-    themeList = themeList.split(",");
-  }
-  const indexOfDomain = themeList.indexOf(domain);
-  let themeCSS = document.querySelector("#spantree-theme");
-  if (indexOfDomain > -1) {
-    themeList.splice(indexOfDomain, 1);
-    document.querySelector("#spantree-theme").disabled = true;
-  }else{
-    themeList.push(domain);
+  let themeList = JSON.parse(localStorage.getItem("spantree-themelist")) || {};
+
+  if (domain in themeList) {
+    const alteredValue = !themeList[domain];
+    themeList[domain] = alteredValue;
+    document.querySelector("#spantree-theme").disabled = !alteredValue;
+  } else {
+    themeList[domain] = true;
     document.querySelector("#spantree-theme").disabled = false;
   }
-  localStorage.setItem("spantree-themelist", themeList.toString());
+
+  localStorage.setItem("spantree-themelist", JSON.stringify(themeList));
 };
