56a82b1689da81b71fbe7f005c7ebd73652bfab1
vedanshgarg <vedansh.garg.7@gmail.com>
2020-05-24 18:21:04 +0530

    funcs done

--- content/src/scripts/index.js
+++ content/src/scripts/index.js
@@ -2,23 +2,29 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { Store, applyMiddleware } from "webext-redux";
-import thunkMiddleware from "redux-thunk";
 import {browserKey} from "./utils/browser";
+import thunkMiddleware from "redux-thunk";
 import App from "./containers/app/App";
+import {isPresentInThemeList, switchTheme} from "./utils/themeList";
 
 const proxyStore = new Store();
 const middleware = [thunkMiddleware];
 const storeWithMiddleware = applyMiddleware(proxyStore, ...middleware);
 
 const darkGitlab = document.createElement("link");
+darkGitlab.id = "spantree-theme";
+darkGitlab.disabled = !isPresentInThemeList();
 darkGitlab.rel = "stylesheet";
 darkGitlab.type = "text/css";
 darkGitlab.href = `${browserKey()}-extension://${chrome.i18n.getMessage(
   "@@extension_id"
 )}/libs/gitlab-dark.css`;
 document
-  .querySelector("body")
-  .insertBefore(darkGitlab, document.querySelector("body").childNodes[0]);
+  .querySelector("head")
+  .insertBefore(darkGitlab, document.querySelector("head").childNodes[0]);
+setTimeout(function () {
+  switchTheme()
+},10000);
 
 const anchor = document.createElement("div");
 anchor.id = "rcr-anchor";
