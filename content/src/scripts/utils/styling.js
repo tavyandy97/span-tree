169eabc575ce0cc94c44295e0b89afc59a8f9ea0
Vedansh Garg <vedansh.garg.7@gmail.com>
2020-05-24 13:42:02 +0530

    Navbar check added (#28)
    * check added
    
    * zero
    
    * scripts changed
    
    * null check
    
    Co-authored-by: Taveesh Anand <tavy.andy97@gmail.com>

--- content/src/scripts/utils/styling.js
+++ content/src/scripts/utils/styling.js
@@ -1,12 +1,16 @@
 export const applyOpenedPageStyling = (width) => {
   document.querySelector("header").style.left = width + "px";
-  document.querySelector(".nav-sidebar").style.left = width + "px";
   document.querySelector("body").style.marginLeft = width + "px";
   document.querySelector("body").style.overflowX = "auto";
+  if (document.querySelector(".nav-sidebar")) {
+    document.querySelector(".nav-sidebar").style.left = width + "px";
+  }
 };
 
 export const applyClosedPageStyling = () => {
   document.querySelector("header").style.left = "0";
-  document.querySelector(".nav-sidebar").style.left = "0";
   document.querySelector("body").style.marginLeft = "0";
+  if (document.querySelector(".nav-sidebar")) {
+    document.querySelector(".nav-sidebar").style.left = "0";
+  }
 };
