1101f8b81c9e56b420dafed9049da9ec543f89fa
vedanshgarg <vedansh.garg.7@gmail.com>
2020-06-01 19:13:56 +0530

    split options

---
+++ popup/src/scripts/components/Options/CompatibilityMode/CompatibilityMode.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.css";
+
+function CompatibilityMode() {
+  return (
+    <div className="option">
+      Compatibility Mode <input type="checkbox" />
+    </div>
+  );
+}
+
+export default CompatibilityMode;
--- popup/src/scripts/components/Options/Options.jsx
+++ popup/src/scripts/components/Options/Options.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
 import "./styles.css";
+import CompatibilityMode from "./CompatibilityMode";
 
 function Options() {
   return (
     <div className="options">
-      <div className="option">
-        Compatibility Mode <input type="checkbox"/>
-      </div>
+      <CompatibilityMode />
     </div>
   );
 }
