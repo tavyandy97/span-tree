f653f32289730ddb1cade74ba3a14a090074065e
chen <yurenchen@yeah.net>
2023-07-13 04:11:19 +0800

    U event/axios.js: fix gitlab baseURL for site at sub dir

--- event/axios.js
+++ event/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const base_url = window.location.origin + (document.head.innerHTML.match(/;gon.relative_url_root="(\/\w+?)";/) || [,''])[1];
+console.log('---spantree:', base_url);
+
 const options = {
-  baseURL: `${window.location.origin}/api/v4/projects/`,
+  baseURL: `${base_url}/api/v4/projects/`,
 };
 
 const inst = axios.create(options);
