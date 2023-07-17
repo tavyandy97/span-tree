0d44dfe5d494c261164196c2af04b1ea6bcbfe09
chen <yurenchen@yeah.net>
2023-07-17 22:16:32 +0800

    U event/axios.js: comment out debug log

--- event/axios.js
+++ event/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const base_url = window.location.origin + (document.head.innerHTML.match(/;gon.relative_url_root="(\/\w+?)";/) || [,''])[1];
-console.log('---spantree:', base_url);
+// console.log('---spantree:', base_url);
 
 const options = {
   baseURL: `${base_url}/api/v4/projects/`,
